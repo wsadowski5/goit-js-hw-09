bf681b4151157c2e7b81b3e1ef280d6506d53465
wsadowski5 <130093777+wsadowski5@users.noreply.github.com>
2023-09-22 17:55:26 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const body = document.querySelector('body')
+const btnStart = document.querySelector('[data-start]')
+const btnStop = document.querySelector('[data-stop')
+
+btnStop.toggleAttribute('disabled',true)
+
+let timerId = null
+
+const btnDisabledToggler = () => {
+    btnStop.toggleAttribute('disabled',);
+    btnStart.toggleAttribute('disabled',);
+    
+}
+
+btnStart.addEventListener('click', () => {
+    btnDisabledToggler()
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    },1000)
+}
+)
+
+btnStop.addEventListener ('click', () => {
+    btnDisabledToggler();
+    clearInterval(timerId);
+})
+
+
