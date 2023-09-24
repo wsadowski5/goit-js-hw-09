40433a76392ac40f8e3291b1ea9156a1bab42755
wsadowski5 <130093777+wsadowski5@users.noreply.github.com>
2023-09-24 12:31:32 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    console.log(`Fulfilled promise ${position} in ${delay}ms`)
   } else {
-    // Reject
+    console.log(`Rejected promise ${position} in ${delay}ms`)
   }
 }
+
+
+const firstDelay = document.querySelector('input[name="delay"]')
+const delayStep = document.querySelector('input[name="step"]')
+const amount = document.querySelector('input[name="amount"]')
+const createPromiseBtn = document.querySelector('button[type=submit]')
+
+
+const createHandler = (event) => {
+  event.preventDefault()
+  for ( let i = 1; i <= amount.value; i++ ) {
+    let position = i
+    let delay = Number(firstDelay.value) + Number(delayStep.value*(i-1));
+
+      setTimeout(() => {
+        createPromise(position,delay)
+      }, delay)
+    
+ }
+}
+
+createPromiseBtn.addEventListener('click',createHandler)
+
